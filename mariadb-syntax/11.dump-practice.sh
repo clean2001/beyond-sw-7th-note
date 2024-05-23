80118cd005b3dc991634772d8e12e71da22cf02b
kimsejeong <clean01@hanyang.ac.kr>
2024-05-23 18:31:40 +0900

    [day 8]: mariadb view, procedure

--- mariadb-syntax/11.dump-practice.sh
+++ mariadb-syntax/11.dump-practice.sh
@@ -3,3 +3,13 @@ mysqldump -u root -p --default-character-set=utf8mb4 board > dumpfile.sql
 
 # 한글이 깨질 때 -> -r 옵션 주기
 mysqldump -u root -p board -r dumpfile.sql
+
+# 리눅스에서 마리아디비 서버 설치
+sudo apt-get install mariadb-server
+
+# 마리아디비 서버 시작
+sudo systemctl start mariadb
+
+
+# 덤프파일 옮기기
+mysql -u root -p board < dumpfile.sql
